91b07c70caa19ae3e03367f27b10a6204cc043fc
Nikita Savenko <nikita.savenko.dev@gmail.com>
2023-10-22 12:36:28 +0300

    Add branch feature-header, and changes

---
+++ client/src/hooks/useSwitchTheme.ts
@@ -0,0 +1,60 @@
+import { useEffect, useState } from "react";
+
+export function useSwitchTheme() {
+  const [isDarkTheme, setIsDarkTheme] = useState<boolean>();
+
+  const changeClassThemeHtml = (value: string) => {
+    const elementHtml: HTMLElement | null = document.getElementById("html");
+
+    if (elementHtml) {
+      const classListHtml: DOMTokenList | undefined = elementHtml.classList;
+      switch (value) {
+        case "dark":
+          classListHtml.remove("light");
+          classListHtml.add(value);
+          break;
+
+        case "light":
+          classListHtml.remove("dark");
+          classListHtml.add(value);
+          break;
+
+        default:
+          classListHtml.add(value);
+          console.log("Error, this theme does not exist!");
+          break;
+      }
+    }
+  };
+
+  const changeTheme = (isDark: boolean) => {
+    const themeName: string = isDark ? "dark" : "light";
+
+    setIsDarkTheme(isDark);
+    changeClassThemeHtml(themeName);
+    localStorage.setItem("theme", themeName);
+  };
+
+  useEffect(() => {
+    const theme: string | null = localStorage.getItem("theme");
+
+    if (theme === "dark") {
+      setIsDarkTheme(true);
+      changeClassThemeHtml(theme);
+    }
+
+    if (theme === "light") {
+      setIsDarkTheme(false);
+      changeClassThemeHtml(theme);
+    }
+
+    if (theme === null) {
+      const isDarkTheme = window.matchMedia(
+        "(prefers-color-scheme: dark)",
+      ).matches;
+      changeTheme(isDarkTheme);
+    }
+  }, []);
+
+  return [isDarkTheme, changeTheme] as const;
+}
