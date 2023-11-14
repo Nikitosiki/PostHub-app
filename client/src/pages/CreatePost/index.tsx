200430a720c3ac498664937727b43f22d4f3d8a6
Nikita Savenko <nikita.lol.d9@gmail.com>
2023-11-15 00:25:27 +0200

    Page Create Post

--- client/src/pages/CreatePost/index.tsx
+++ client/src/pages/CreatePost/index.tsx
@@ -1,7 +1,22 @@
-import { Card, CardBody, CardHeader, Divider, Input } from "@nextui-org/react";
+import { useState, ChangeEvent } from "react";
+import {
+  Button,
+  Card,
+  CardBody,
+  CardFooter,
+  CardHeader,
+  Input,
+} from "@nextui-org/react";
+
 import Editor from "src/components/Editor";
 
 const CreatePost = () => {
+  const [inputLength, setInputLength] = useState<number>(0);
+
+  const handleInputChange = (event: ChangeEvent<HTMLInputElement>) => {
+    setInputLength(event.currentTarget.value.length);
+  };
+
   return (
     <>
       <Card
@@ -10,14 +25,35 @@ const CreatePost = () => {
       >
         <CardHeader className="flex flex-col">
           <h1 className="w-full text-left text-lg">Create post</h1>
-          <Divider className="my-2" />
+          {/* <Divider className="my-2" /> */}
         </CardHeader>
         <CardBody>
-          <Input variant="bordered" label="Title" classNames={"input"} />
+          <Input
+            onChange={handleInputChange}
+            variant="bordered"
+            label="Title"
+            classNames={{
+              input: "",
+              inputWrapper: "shadow-none border-default-200/75",
+            }}
+            endContent={
+              <div className="pointer-events-none flex items-center">
+                <span className="text-small text-default-400">
+                  {inputLength}/300
+                </span>
+              </div>
+            }
+          />
         </CardBody>
         <CardBody>
           <Editor />
         </CardBody>
+        <CardFooter className="justify-end">
+          <div className="flex flex-row gap-4">
+            <Button>Cancel</Button>
+            <Button color="primary">Public</Button>
+          </div>
+        </CardFooter>
       </Card>
     </>
   );
