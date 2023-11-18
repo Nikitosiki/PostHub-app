fbdacc491785aae0860186f7909953b3d1fb40cc
Nikita Savenko <nikita.lol.d9@gmail.com>
2023-11-18 11:25:36 +0200

    feat: add comment editor component

---
+++ client/src/components/EditorComment/index.tsx
@@ -0,0 +1,43 @@
+import { FC } from "react";
+import { Editor as EditorTiny, IAllProps } from "@tinymce/tinymce-react";
+
+import { useSwitchTheme } from "src/hooks";
+import "./style.css";
+
+const EditorComment: FC<IAllProps> = (props) => {
+  const [isDarkTheme] = useSwitchTheme();
+
+  return (
+    <EditorTiny
+      apiKey={import.meta.env.VITE_TinyMCE_apiKey}
+      init={{
+        // height: 500,
+        menubar: false,
+        forced_root_block: "div",
+        skin: isDarkTheme ? "oxide-dark" : "oxide",
+        content_css: isDarkTheme ? "dark" : "default",
+        plugins: [
+          // "accordion",
+          // "preview",
+          "lists",
+          "advlist",
+          "wordcount",
+          "emoticons",
+          "help",
+          "paste",
+          "link",
+          "autolink",
+          // "code",
+        ],
+        toolbar:
+          "undo redo | styles bold italic underline strikethrough | " + //align
+          "bullist numlist | " + // outdent indent accordion
+          "forecolor backcolor emoticons |" +
+          "help", // preview code
+      }}
+      {...props}
+    />
+  );
+};
+
+export default EditorComment;
