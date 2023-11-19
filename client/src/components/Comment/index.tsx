7f88291f08144ba7d22a1cd0cb277f8145737b49
Nikita Savenko <nikita.lol.d9@gmail.com>
2023-11-19 15:37:16 +0200

    feat: add comment component

---
+++ client/src/components/Comment/index.tsx
@@ -0,0 +1,42 @@
+import { FC, ReactNode } from "react";
+import { Link } from "react-router-dom";
+import { Avatar } from "@nextui-org/react";
+
+import { IComment } from "src/interfaces";
+import { getShortFormattedDate, nullToUndefined } from "src/utils";
+
+interface ICommentProps {
+  comment: IComment;
+  children?: ReactNode;
+}
+
+const Comment: FC<ICommentProps> = ({ comment, children }) => {
+  return (
+    <>
+      <div className="mb-2 flex flex-row gap-2">
+        <Link to={`/author/${comment.author.id}`}>
+          <Avatar
+            size="sm"
+            name={comment.author.name}
+            src={nullToUndefined(comment.author.image_url)}
+          />
+        </Link>
+        <div>
+          <div className="text-sm">
+            <Link to={`/author/${comment.author.id}`}>
+              <span>{comment.author.name}</span>
+            </Link>
+            <span className="m-1 text-default-400">𐄁</span>
+            <span className="text-default-400">
+              {getShortFormattedDate(comment.created_at)}
+            </span>
+          </div>
+          <p>{comment.content}</p>
+          <div className="mt-2">{children}</div>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default Comment;
