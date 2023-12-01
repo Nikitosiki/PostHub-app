8e1526e252ad67e145a3542788740ab8e15fc610
Nikita Savenko <nikita.lol.d9@gmail.com>
2023-12-01 19:10:23 +0200

    add: content to hots page

--- client/src/api/supabase/post.ts
+++ client/src/api/supabase/post.ts
@@ -76,6 +76,30 @@ export const getNewPosts = async (
     .filter((post) => post !== null) as IPosts;
 };
 
+export const getHotPosts = async (
+  pageNumber: number,
+  pageSize: number = 10,
+): Promise<IPost[]> => {
+  console.log(pageNumber, pageSize);
+  const { data, error } = await client
+    .from("posts")
+    .select(
+      ` *, 
+      users!posts_author_id_fkey(*, genders(name)), 
+      tags(*, users(*, genders(name))), 
+      reactions(*)`,
+    )
+    .order("count_view", { ascending: false })
+    .range(pageNumber * pageSize - pageSize, pageNumber * pageSize - 1);
+
+  error && console.log(error);
+  if (!Array.isArray(data) || data.length < 1) return [];
+
+  return data
+    .map((post) => toPost(post))
+    .filter((post) => post !== null) as IPosts;
+};
+
 export const getPostById = async (id: string): Promise<IPost | null> => {
   const { data, error } = await client
     .from("posts")
--- client/src/pages/Hots/index.tsx
+++ client/src/pages/Hots/index.tsx
@@ -1,17 +1,37 @@
-import Post from "src/modules/Post";
-import Search from "src/components/Search";
+import { useState } from "react";
 
-import { getHotPosts } from "src/api/preview";
+import Post from "src/modules/Post";
+import { IPost } from "src/interfaces";
+import { getHotPosts } from "src/api/supabase/post";
+import InfiniteScrollWrapper from "src/components/InfiniteScrollWrapper";
 
 const Hots = () => {
+  const [posts, setPosts] = useState<IPost[]>([]);
+  const [hasMorePosts, setHasMorePosts] = useState<boolean>(true);
+  const [numberPage, setNumberPage] = useState<number>(1);
+  const postsOnPage = 3;
+
+  const getNextPosts = async () => {
+    const nextPosts = await getHotPosts(numberPage, postsOnPage);
+    if (nextPosts.length === 0) setHasMorePosts(false);
+    setPosts([...posts, ...nextPosts]);
+    setNumberPage(numberPage + 1);
+  };
+
   return (
     <>
-      <div className="flex w-full flex-col gap-4 p-2">
-        <Search />
-        {getHotPosts().map((post) => (
-          <Post key={post.id} post={post} />
-        ))}
-      </div>
+      <InfiniteScrollWrapper
+        dataLength={posts.length}
+        next={getNextPosts}
+        hasMore={hasMorePosts}
+        loader={""}
+      >
+        <div className="flex w-full flex-col gap-4 p-2">
+          {posts.map((post) => (
+            <Post key={post.id} post={post} />
+          ))}
+        </div>
+      </InfiniteScrollWrapper>
     </>
   );
 };
